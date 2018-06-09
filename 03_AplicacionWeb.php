d913440e0c833cb3203c9c03598022fe55b7714d
Brian Sandoval <briansandovalsoto@gmail.com>
2018-06-09 09:44:59 -0500

    Lectura de un archivo con traductor

--- 03_AplicacionWeb.php
+++ 03_AplicacionWeb.php
@@ -36,6 +36,27 @@
         }
         return $text;
     }
+
+    function translateByFile(string $lang = "en", string $text) : string {
+        $fr = fopen('traductor.txt', 'r');
+        $respose = "";
+
+        while(false !== ( $line = fgetcsv($fr)) ){
+            $line[1] = $line[1] ?? null;
+            if(trim($line[0]) === trim($lang))
+                if(trim($line[1]) == trim($text) )
+                    $respose = trim($line[2]);
+        }
+
+        fclose($fr);
+
+        if($respose != "" && $respose != null) {
+            return $respose;
+        }else{
+            return $text;
+        }
+    }
+
     ?>
 
     <!doctype html>
@@ -49,5 +70,6 @@
     </head>
     <body>
         <h1> <?php echo translate($lang,"Hello Sandoval"); ?></h1>
+        <h2> <?php echo translateByFile($lang,"Hello Sandoval"); ?></h2>
     </body>
     </html>
---
+++ 04_ReadFileTraductor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kmmx02-ubuntu
+ * Date: 9/06/18
+ * Time: 09:13 AM
+ */
+
+    $t = translateByFile("es", "hello");
+    var_dump($t);
+    function translateByFile(string $lang = "en", string $text) : string {
+        $fr = fopen('traductor.txt', 'r');
+        $respose = "";
+
+        while(false !== ( $line = fgetcsv($fr)) ){
+            $line[1] = $line[1] ?? null;
+            if(trim($line[0]) === trim($lang))
+                if(trim($line[1]) == trim($text) )
+                    $respose = trim($line[2]);
+        }
+
+        fclose($fr);
+
+        if($respose != "" && $respose != null) {
+            return $respose;
+        }else{
+            return $text;
+        }
+    }
